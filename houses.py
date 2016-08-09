031a36d66a1236ac40aff8a04589f16536aade61
Phill Conrad <pconrad@cs.ucsb.edu>
2016-08-09 10:03:01 -0700

    WIP PC/AT/RB progress, but draw house still broken

--- houses.py
+++ houses.py
@@ -1,52 +1,77 @@
 import turtle
 import math    
 
-def drawHouse(t):
+def calculateHypotenuse(a,b):
+    ''' if a and b are sides of a right triangle, calculate c, the hypotenuse '''
+    return math.sqrt(a**2 + b**2)
+
+
+def drawHouse(t, houseColor,penSize,height,width):
     ''' draw a house '''
 
+    t.pensize(penSize)
+    t.down()
+    t.color(houseColor)
+
     # draw the base
-    t.forward(100)
+    t.forward(width)
 
     # draw the right wall
     t.left(90)
-    t.forward(50)
+    t.forward(height)
 
     # draw the line between 1st story and roof
     t.left(90)
-    t.forward(100)
+    t.forward(width)
 
     # draw the left wall
     t.left(90)
-    t.forward(50)
+    t.forward(height)
 
     # backup and turn around
-    t.backward(50)
+    t.backward(height)
     t.left(180)
 
     # Go to peak of roof.   Its a 45 degree angle, so its
     # is sqrt(2) times 50 long
 
-    t.right(45)
-    t.forward(math.sqrt(2.0) * 50)
+    roofAngleInRadians = math.atan( height / (width/2.0) )
+    roofAngleInDegrees = math.degrees(roofAngleInRadians)
+
+    roofLength = calculateHypotenuse(height, width/2.0)
+
+    print "roofAngleInDegrees=",roofAngleInDegrees
+
+    t.right(roofAngleInDegrees)
+    t.forward(roofLength)
 
     # rest of roof
     
-    t.right(90)
-    t.forward(math.sqrt(2.0) * 50)
+    t.right(90)  # Rachel thinks this is incorrct (she's right)
+    t.forward(roofLength)
 
     # reset turtle to bottom right, facing east
 
-    t.right(45)
-    t.forward(50)
+    t.right(roofAngleInDegrees)  # Andrew T. thinks this is incorrect (he's right)
+    t.forward(height)
     t.left(90)
+    
     
 
 def drawStuff(t):
     ''' draw some example houses '''
-    
-    drawHouse(t)
 
-    drawHouse(t)
+    myColors=["red","purple","turquoise","green","pink","orange"]
+    t.goto(-300,0)
+    for i in range(6):
+      print "drawing house number ",i
+      drawHouse(t, myColors[i], 5+(i*2), 50, 100)
+
+    t.up()
+    t.goto(-300,-200)
+
+    
+    drawHouse(t, "black", 5, 33, 200)
 
 
 if __name__ == "__main__":
